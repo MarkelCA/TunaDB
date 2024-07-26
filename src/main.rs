fb02f083a725c2a6368edeb4ed03fc0624f710ce
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-07-26 15:00:39 +0200

    Created get and set commands

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,30 @@ use clap::Parser;
 struct CliArgs {
     #[clap(short)]
     file_name: String,
-    key: String,
-    value: String,
+    #[clap(subcommand)]
+    command: Command,
 }
 
 mod storage;
+
+#[derive(Parser, Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: String },
+}
 
 fn main() {
     let args = CliArgs::parse();
 
     let mut engine = storage::new_engine(&args.file_name);
-    // engine.set(&args.key, &args.value);
-    match engine.get(&args.key) {
-        Some(value) => println!("{}: {}", args.key, value),
-        None => println!("{} not found", args.key),
+
+    match args.command {
+        Command::Get { key } => match engine.get(&key) {
+            Some(value) => println!("{}: {}", key, value),
+            None => println!("{} not found", key),
+        },
+        Command::Set { key, value } => {
+            engine.set(&key, &value);
+        }
     }
 }
