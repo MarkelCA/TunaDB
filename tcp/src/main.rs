6c3bc7e43872e21190a2b13b188e8f16f6905a0a
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-08-14 19:31:33 +0200

    Fixed & improved tcp implementation, also added error handling

--- tcp/src/main.rs
+++ tcp/src/main.rs
@@ -1,8 +1,11 @@
+use anyhow::anyhow;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 use core::config;
 use core::storage::{self, Engine, EngineEnum};
+use std::error::Error;
+use std::str::SplitWhitespace;
 
 #[path = "./storage.rs"]
 mod tcp_storage;
@@ -33,36 +36,72 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 };
 
                 // Write the data back
-                let command = std::str::from_utf8(&buf[0..n]).unwrap();
-                let command = command.split(' ').nth(0).unwrap();
-                println!("Received command: {}", command);
-                let response = run_command(&mut engine, &command); // Pass a reference to the engine
+                let command = std::str::from_utf8(&buf[0..n]);
 
-                if let Err(e) = socket.write_all(response.as_bytes()).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
+                if let Err(e) = command {
+                    if let Err(e) = socket.write_all(e.to_string().as_bytes()).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+                    continue;
+                }
+                // We can use unwrap here because the error is handled above
+                let tokens = command.unwrap().split_whitespace();
+                let response = run_command(&mut engine, tokens); // Pass a reference to the engine
+
+                match response {
+                    Ok(response) => {
+                        if let Err(e) = socket.write_all(response.as_bytes()).await {
+                            eprintln!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
+                    }
+                    Err(e) => {
+                        if let Err(e) = socket
+                            .write_all(format!("{}\n", e.to_string()).as_bytes())
+                            .await
+                        {
+                            eprintln!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
+                    }
                 }
             }
         });
     }
 }
 
-fn run_command(engine: &mut EngineEnum, command: &str) -> String {
+fn run_command(engine: &mut EngineEnum, mut tokens: SplitWhitespace) -> anyhow::Result<String> {
+    let command = tokens.nth(0).ok_or(anyhow!("Command not found"))?;
     match command {
-        "get" => match engine.get("key") {
-            Ok(value) => value.unwrap_or("(nil)\n".to_string()),
-            Err(e) => format!("error: {}\n", e),
+        "get" => match engine.get(
+            tokens
+                .nth(0)
+                .ok_or(anyhow!("Key not found in get command"))?,
+        ) {
+            Ok(value) => match value {
+                Some(v) => Ok(format!("{}\n", v)),
+                None => Ok("(nil)\n".to_string()),
+            },
+            Err(e) => Ok(format!("error: {}\n", e)),
         },
-        "set" => match engine.set("key", "value") {
-            Ok(_) => "ok\n".to_string(),
-            Err(e) => format!("error: {}", e),
+        "set" => match engine.set(
+            tokens
+                .nth(0)
+                .ok_or(anyhow!("Key not found in set command"))?,
+            tokens
+                .nth(0)
+                .ok_or(anyhow!("Value not found in set command"))?,
+        ) {
+            Ok(_) => Ok("ok\n".to_string()),
+            Err(e) => Ok(format!("error: {}", e)),
         },
         "list" => {
             let mut result = String::new();
             match engine.list() {
                 Ok(keys) => {
                     for key in keys {
-                        result.push_str(&format!("{}\n", key));
+                        result.push_str(&format!("- {}\n", key));
                     }
                     result.push_str("\n");
                 }
@@ -70,8 +109,9 @@ fn run_command(engine: &mut EngineEnum, command: &str) -> String {
                     result.push_str(&format!("error: {}\n", e));
                 }
             }
-            result
+            Ok(result)
         }
-        _ => "unknown command\n".to_string(),
+        "help" => Ok("Commands:\nget <key>\nset <key> <value>\nlist\n".to_string()),
+        _ => Ok("unknown command\n".to_string()),
     }
 }
